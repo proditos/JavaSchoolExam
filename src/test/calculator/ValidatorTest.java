165fb834e1ada1a867773798e2d1aab7826d7a74
Vladislav Konovalov <konovalov.phys@gmail.com>
2022-10-21 17:19:22 +0300

    Add test values

--- src/test/calculator/ValidatorTest.java
+++ src/test/calculator/ValidatorTest.java
@@ -15,7 +15,7 @@ class ValidatorTest {
     private final Validator validator = new ValidatorImpl();
 
     @ParameterizedTest
-    @ValueSource(strings = {"1", "-1", "1.2", "+1.0", "-10/(+2-7+3)*4", "+10/(((-2-7)+3)*4)"})
+    @ValueSource(strings = {"1", "-1", "1.2", "+1.0", "-10/(+2-7+3)*4", "+10/(((-2-7)+3)*4)", "1+1"})
     void itShouldReturnTrueForCorrectExpressions(String expr) {
         assertTrue(validator.isValid(expr));
     }
@@ -27,7 +27,7 @@ class ValidatorTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"+", "*", ".", "(", ")", "()", "*1+2", "1+.2", "1+2.", "1..2+3",
+    @ValueSource(strings = {"+", "*", ".", "(", ")", "()", "*1+2", "1+.2", ".1", "1+2.", "1..2+3",
             "1.23.4+5", "1++2", "(1+2)(3+4)", "1 + 2", "1+2,3", "(1*(2+3)", "1+2+", "1*(2+3))"})
     void isShouldReturnFalseForIncorrectExpressions(String expr) {
         assertFalse(validator.isValid(expr));
